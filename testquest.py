681852c3e797d0872b441d4a512d7e3cc9ee37bd
sid <sid.pittie@gmail.com>
2017-11-14 22:13:31 -0500

    Fail

--- testquest.py
+++ testquest.py
@@ -1,2 +1,10 @@
 import requests
+from requests.auth import HTTPBasicAuth
 
+r = requests.get('https://instruct.math.lsa.umich.edu/webwork2/ma115-065-f17/')
+
+payload = {'username':'spittie', 'password':"The aston db11 is amazing!"}
+
+r = requests.post('https://instruct.math.lsa.umich.edu/webwork2/ma115-065-f17/', auth=HTTPBasicAuth('spittie', 'The aston db11 is amazing!'))
+
+print (r.url)
